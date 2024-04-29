8ac455d8fc8efc2f7beebcab4f61762bd76cdbd4
Alexander Link <alex@linkzuhause.com>
2024-04-29 21:50:34 +0200

    changed uuid of energystatetext

--- src/characteristics/senec-energy-state-text.ts
+++ src/characteristics/senec-energy-state-text.ts
@@ -4,7 +4,7 @@ export = (homebridge : API ) => {
   const Charact = homebridge.hap.Characteristic;
 
   return class SenecEnergyStateText extends Charact {
-    public static readonly UUID: string = '43a367de-4059-484f-9bdc-761b60d75fcb';
+    public static readonly UUID: string = '5a96070c-d0ef-4dc6-ad1d-118a6b5c6147';
     public static readonly DISPLAY_NAME = 'Energy State';
     constructor() {
       super(SenecEnergyStateText.DISPLAY_NAME, SenecEnergyStateText.UUID, {
